93c9fffe8338d617efc31cc4c145a361f668a908
Adam Bedient <adambedient@gmail.com>
2024-05-17 14:00:16 -0400

    Implement flower components

---
+++ src/components/FlowerAssembly/FlowerAssembly.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import Receptacle from "./Receptacle"
+import Stem from "./Stem"
+
+
+export default function FlowerAssembly({ flower }) {
+    const [planted, setPlanted] = useState(Date.now())
+    const [currentTime, setCurrentTime] = useState(null)
+    const [topPoint, setTopPoint] = useState(null)
+    const [bloomAngle, setBloomAngle] = useState(null)
+    // const [thisFlower, setThisFlower] = useState(convertPlantObj(plant))
+    const [lifeCycle, setLifeCycle] = useState(300000)
+    const [stage, setStage] = useState('seedling')
+
+    const stages = ['seedling', 'blooming', 'thriving', 'wilting', 'dead']
+    const timeCalc = (Date.now() - planted) / 5000
+
+    useEffect(() => {
+        if(flower){
+            setStage(stages[0])
+        }
+    },  [flower])
+
+    const handleTopPoint = (point, angle) => {
+            setTopPoint(point)
+            setBloomAngle(angle)
+        }
+
+    return (
+        <>
+            {thisFlower && <Receptacle topPoint={topPoint} bloomAngle={bloomAngle} flower={flower.phases[stage]}/>}
+            <Stem onTopPointComputed={handleTopPoint} flower={flower.phases[stage]}/>
+        </>
+    )
+}
--- src/components/Flowers/Flowers.js
+++ src/components/Flowers/Flowers.js
@@ -1,10 +1,12 @@
+import FlowerAssembly from "../FlowerAssembly/FlowerAssembly"
 import { StyledFlowers } from "./Flowers.styled";
 import { useState } from 'react'
 
 export default function Flowers({myFlowers}) {
 
     const flowers = myFlowers.map((flower) => {
-        return <div className="flower"></div>
+        return <FlowerAssembly flower={flower} />
+        // return <div className="flower"></div>
     })
 
     return (
